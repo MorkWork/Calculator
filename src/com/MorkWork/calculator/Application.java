eadfe1706eddfb7d1c3ed7c7d7dd9d3e94302ded
MorkWork <ddogerman@gmail.com>
2022-10-27 21:45:09 +0300

    fixes

---
+++ src/com/MorkWork/calculator/Application.java
@@ -0,0 +1,7 @@
+package com.MorkWork.calculator;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        new CalculatorIO();
+    }
+}
--- src/com/MorkWork/calculator/Calculator.java
+++ src/com/MorkWork/calculator/Calculator.java
@@ -3,54 +3,21 @@ package com.MorkWork.calculator;
 import java.math.BigDecimal;
 
 public class Calculator {
-    public static BigDecimal calculate(BigDecimal num1, MathOperation operation, BigDecimal num2) throws Exception {
-        BigDecimal result = null;
+    public BigDecimal calculate(BigDecimal num1, MathOperation operation, BigDecimal num2) throws Exception {
         switch (operation) {
             case ADDITION:
-                result = num1.add(num2);
-                break;
+                return num1.add(num2);
             case SUBSTRACTION:
-                result = num1.subtract(num2);
-                break;
-            case MILTIPLYCATION:
-                result = num1.multiply(num2);
-                break;
-            case DIVIDE:
+                return num1.subtract(num2);
+            case MULTIPLICATION:
+                return num1.multiply(num2);
+            case DIVISION:
                 if (num2.equals(0)) {
                     throw new Exception("Division by zero!!");
                 } else {
-                    result = num1.divide(num2);
+                    return num1.divide(num2);
                 }
-                break;
-
-        }
-        return result;
-    }
-
-
-    enum MathOperation {
-        ADDITION("+"),
-        SUBSTRACTION("-"),
-        MILTIPLYCATION("*"),
-        DIVIDE("/");
-
-        private String title;
-
-        MathOperation(String title) {
-            this.title = title;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public static MathOperation resolve(String operation) throws Exception {
-            for (MathOperation i : MathOperation.values()) {
-                if (operation.equals(i.title)) {
-                    return i;
-                }
-            }
-            throw new Exception("No such operation!!");
+            default: throw new Exception("No such operation!!");
         }
     }
 
